c7898490cc1ad93c719e77c3fc50e3e5bf0ab676
Aleksei Amosov <amos8moses@gmail.com>
2020-08-14 17:02:18 +0300

    added redux, redux-thunk, redux-logger and wrote reducers, actions and store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { rootReducer } from "./redux/rootReducer";
+import {
+  increment,
+  decrement,
+  asyncIncrement,
+  changeTheme,
+} from "./redux/actions";
 import "./styles.css";
 
 const counter = document.getElementById("counter");
@@ -6,31 +16,39 @@ const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-let state = 0;
-
-function render() {
-  counter.textContent = state.toString();
-}
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 addBtn.addEventListener("click", () => {
-  state++;
-  render();
+  store.dispatch(increment());
 });
 
 subBtn.addEventListener("click", () => {
-  state--;
-  render();
+  store.dispatch(decrement());
 });
 
 asyncBtn.addEventListener("click", () => {
-  setTimeout(() => {
-    state++;
-    render();
-  }, 2000);
+  store.dispatch(asyncIncrement());
 });
 
 themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  const newTheme = document.body.classList.contains("light") ? "dark" : "light";
+  store.dispatch(changeTheme(newTheme));
 });
 
-render();
+store.subscribe(() => {
+  const state = store.getState();
+
+  counter.textContent = state.counter;
+  document.body.className = state.theme.value;
+  [addBtn, subBtn, themeBtn, asyncBtn].forEach(
+    (btn) => (btn.disabled = state.theme.disabled)
+  );
+});
+
+store.dispatch({ type: "INIT_APPLICATION" });
